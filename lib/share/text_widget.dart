265c4a671472f92f56225785aa210481a5883237
kakelay <kakelay18052002@gmail.com>
2023-11-25 23:41:57 +0700

    first commit

---
+++ lib/share/text_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+ 
+
+Text TextWidget(
+  String text, {
+  double fontSize = 14,
+  FontWeight weight = FontWeight.normal,
+  Color? color,
+  TextAlign align = TextAlign.left,
+}) {
+  color ??= Colors.black;
+  return Text(
+    text,
+    textAlign: align,
+    style: TextStyle(
+      fontSize: fontSize,
+      color: color,
+      fontWeight: weight,
+    ),
+  );
+}
